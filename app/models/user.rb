57c2c9844a8b02f970e484266d3bd139dc09e28a
Grady Shelton and Monique St. Laurent <monique@>
2017-07-06 10:32:02 -0700

    increase attack level based on items

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :user_items
+
+
+  def attack_level_math
+    items = []
+    attack_increase = 0
+    self.user_items.each {|ui| items.push(Item.find(ui.item_id))}
+    items.each do |item|
+      attack_increase += item.user_attack_increase
+    end
+    self.attack_level + attack_increase
+  end
 end
